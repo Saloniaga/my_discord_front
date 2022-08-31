245e6cf0bdd6a1b28aa0c224b4d7249df5db801c
Saloniaga <sagarwal9475@gmail.com>
2022-08-31 21:35:51 +0530

    pending friends invitations event emitted

--- src/store/actions/friendsActions.js
+++ src/store/actions/friendsActions.js
@@ -16,12 +16,12 @@ export const getActions = (dispatch) => {
   };
 };
 
-// export const setPendingFriendsInvitations = (pendingFriendsInvitations) => {
-//   return {
-//     type: friendsActions.SET_PENDING_FRIENDS_INVITATIONS,
-//     pendingFriendsInvitations,
-//   };
-// };
+export const setPendingFriendsInvitations = (pendingFriendsInvitations) => {
+  return {
+    type: friendsActions.SET_PENDING_FRIENDS_INVITATIONS,
+    pendingFriendsInvitations,
+  };
+};
 
 // export const setFriends = (friends) => {
 //   return {
