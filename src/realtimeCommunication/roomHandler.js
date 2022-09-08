d699378dae2ae02e5963d343487ab425efdfcce5
Saloniaga <sagarwal9475@gmail.com>
2022-09-09 00:22:13 +0530

    leave room functionality added

--- src/realtimeCommunication/roomHandler.js
+++ src/realtimeCommunication/roomHandler.js
@@ -66,25 +66,25 @@ export const joinRoom = (roomId) => {
   // webRTCHandler.getLocalStreamPreview(audioOnly, successCalbackFunc);
 };
 
-// export const leaveRoom = () => {
-//   const roomId = store.getState().room.roomDetails.roomId;
+export const leaveRoom = () => {
+  const roomId = store.getState().room.roomDetails.roomId;
 
-//   const localStream = store.getState().room.localStream;
-//   if (localStream) {
-//     localStream.getTracks().forEach((track) => track.stop());
-//     store.dispatch(setLocalStream(null));
-//   }
+  // const localStream = store.getState().room.localStream;
+  // if (localStream) {
+  //   localStream.getTracks().forEach((track) => track.stop());
+  //   store.dispatch(setLocalStream(null));
+  // }
 
-//   const screenSharingStream = store.getState().room.screenSharingStream;
-//   if (screenSharingStream) {
-//     screenSharingStream.getTracks().forEach((track) => track.stop());
-//     store.dispatch(setScreenSharingStream(null));
-//   }
+  //   const screenSharingStream = store.getState().room.screenSharingStream;
+  //   if (screenSharingStream) {
+  //     screenSharingStream.getTracks().forEach((track) => track.stop());
+  //     store.dispatch(setScreenSharingStream(null));
+  //   }
 
-//   store.dispatch(setRemoteStreams([]));
-//   webRTCHandler.closeAllConnections();
+  //   store.dispatch(setRemoteStreams([]));
+  //   webRTCHandler.closeAllConnections();
 
-//   socketConnection.leaveRoom({ roomId });
-//   store.dispatch(setRoomDetails(null));
-//   store.dispatch(setOpenRoom(false, false));
-// };
+  socketConnection.leaveRoom({ roomId });
+  store.dispatch(setRoomDetails(null));
+  store.dispatch(setOpenRoom(false, false));
+};
